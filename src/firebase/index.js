53bf1b2d4e76260d517cf4f84a785be13f3c2eef
Jose Duarte <duarte@MacBook-Pro-de-Jose.local>
2024-04-13 12:01:24 -0400

    update guests list

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,3 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+//const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/firestore');
+import {getFirestore, collection, getDocs, query, where} from 'firebase/firestore'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDAzqybVhg6a-UyzlRdFRIyGuj2tL9L-RI",
+  authDomain: "boda-duarte-ruiz.firebaseapp.com",
+  projectId: "boda-duarte-ruiz",
+  storageBucket: "boda-duarte-ruiz.appspot.com",
+  messagingSenderId: "2555323531",
+  appId: "1:2555323531:web:52b1383f0434baeac6e08e"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
--- src/utils/guests.js
+++ src/utils/guests.js
@@ -5,7 +5,8 @@ export const guestsList = [
         nick_name: "Paitooo",
         dependents: [
             {name: "Carlota Marquez "}
-        ]
+        ],
+        outside_country: false
     },
     {
         full_name: "Carlota Marquez",
@@ -13,7 +14,8 @@ export const guestsList = [
         nick_name: "Mamiiii",
         dependents: [
             {name: "José Duarte"}
-        ]
+        ],
+        outside_country: false
     },
     {
         full_name: "Génova Duarte",
@@ -23,7 +25,8 @@ export const guestsList = [
             {name: "Manuel Silva"},
             {name: "Patricia Silva"},
             {name: "Carlos Figueredo"}
-        ]
+        ],
+        outside_country: false
     },
     {
         full_name: "Carlos Figueredo",
@@ -33,7 +36,8 @@ export const guestsList = [
             {name: "Manuel Silva"},
             {name: "Patricia Silva"},
             {name: "Génova Duarte"}
-        ]
+        ],
+        outside_country: false
     },
     {
         full_name: "Génesis Riff",
@@ -41,7 +45,8 @@ export const guestsList = [
         nick_name: "Gena",
         dependents: [
             {name: "Eva Riff"},
-        ]
+        ],
+        outside_country: false
     },
     {
         full_name: "Eva Riff",
@@ -49,7 +54,8 @@ export const guestsList = [
         nick_name: "Evii",
         dependents: [
             {name: "Genésis Riff"},
-        ]
+        ],
+        outside_country: false
     },
     {
         full_name: "Daniel Duarte",
@@ -57,7 +63,8 @@ export const guestsList = [
         nick_name: "Danielito",
         dependents: [
             {name: "Nancy Arcia"},
-        ]
+        ],
+        outside_country: false
     },
     {
         full_name: "Nancy Arcia",
@@ -65,7 +72,8 @@ export const guestsList = [
         nick_name: "Tia Nancy",
         dependents: [
             {name: "Daniel Duarte"},
-        ]
+        ],
+        outside_country: false
     },
     {
         full_name: "Domingo Luis Duarte",
@@ -75,13 +83,15 @@ export const guestsList = [
             {name: "Daniel Enrique Duarte"},
             {name: "Maria Daniela Duarte"},
             {name: "Marianellys Bermudez"},
-        ]
+        ],
+        outside_country: false
     },
     {
         full_name: "Ana María Duarte",
         phone_number: "584248476937",
         nick_name: "Anitica",
-        dependents: []
+        dependents: [],
+        outside_country: false
     },
     {
         full_name: "Benilde Duarte",
@@ -89,7 +99,8 @@ export const guestsList = [
         nick_name: "Tatico",
         dependents: [
             {name: "Carla Spangenberg"}
-        ]
+        ],
+        outside_country: false
     },
     {
         full_name: "Carla Spangenberg",
@@ -97,13 +108,15 @@ export const guestsList = [
         nick_name: "Carlita",
         dependents: [
             {name: "Benilde Duarte"}
-        ]
+        ],
+        outside_country: false
     },
     {
         full_name: "Beride Macuma",
         phone_number: "584248783286",
         nick_name: "Mimi",
-        dependents: []
+        dependents: [],
+        outside_country: false
     },
     {
         full_name: "Adolfo Ruiz",
@@ -111,7 +124,8 @@ export const guestsList = [
         nick_name: "Papito",
         dependents: [
             {name: "Linda Scope"}
-        ]
+        ],
+        outside_country: false
     },
     {
         full_name: "Linda Scope",
@@ -119,13 +133,15 @@ export const guestsList = [
         nick_name: "Linda Linda",
         dependents: [
             {name: "Adolfo Ruiz"}
-        ]
+        ],
+        outside_country: false
     },
     {
         full_name: "Andrea Ruiz",
         phone_number: "584120948960",
         nick_name: "Andreita",
-        dependents: []
+        dependents: [],
+        outside_country: false
     },
     {
         full_name: "Amanda Ruiz",
@@ -133,7 +149,8 @@ export const guestsList = [
         nick_name: "Mandy",
         dependents: [
             {name: "Alejandro Morales"}
-        ]
+        ],
+        outside_country: false
     },
     {
         full_name: "Alejandro Morales",
@@ -141,13 +158,15 @@ export const guestsList = [
         nick_name: "Alejo",
         dependents: [
             {name: "Amanda Ruiz"}
-        ]
+        ],
+        outside_country: false
     },
     {
         full_name: "Alicia Hernandez",
         phone_number: "58x0x0x0x0x0x0x",
         nick_name: "Abuela",
-        dependents: []
+        dependents: [],
+        outside_country: false
     },
     {
         full_name: "Dora Ruiz",
@@ -155,7 +174,8 @@ export const guestsList = [
         nick_name: "Tia Dora",
         dependents: [
             {name: "Oswar Arias"}
-        ]
+        ],
+        outside_country: false
     },
     {
         full_name: "Oswar Arias",
@@ -163,7 +183,8 @@ export const guestsList = [
         nick_name: "Tio Oswar",
         dependents: [
             {name: "Dora Ruiz"}
-        ]
+        ],
+        outside_country: false
     },
     {
         full_name: "Hector Arias",
@@ -172,7 +193,8 @@ export const guestsList = [
         dependents: [
             {name: "Rubeccis Gamboa"},
             {name: "Hector Arias Gamboa"}
-        ]
+        ],
+        outside_country: false
     },
     {
         full_name: "Jesús García",
@@ -182,7 +204,8 @@ export const guestsList = [
             {name: "Omira Millan"},
             {name: "Jesús García Millan"},
             {name: "Amira García Millan"}
-        ]
+        ],
+        outside_country: false
     },
     {
         full_name: "Doris Ruiz",
@@ -192,7 +215,8 @@ export const guestsList = [
             {name: "Alexander Malave"},
             {name: "Dorialis Malave"},
             {name: "Alexandra Malave"}
-        ]
+        ],
+        outside_country: false
     },
     {
         full_name: "Williams Ruiz",
@@ -202,13 +226,15 @@ export const guestsList = [
             {name: "Carmen Rodriguez"},
             {name: "Hugo Ruiz"},
             {name: "Victor Ruiz"}
-        ]
+        ],
+        outside_country: false
     },
     {
         full_name: "Carlyle Scope",
         phone_number: "584144662075",
         nick_name: "Abuelo",
-        dependents: []
+        dependents: [],
+        outside_country: false
     },
     {
         full_name: "Lyncar Scope",
@@ -218,12 +244,365 @@ export const guestsList = [
             {name: "Luis Sandoval"},
             {name: "Lizzie Sandoval"},
             {name: "Lynda Sandoval"}
-        ]
+        ],
+        outside_country: false
     },
     {
         full_name: "Liza Sandoval",
         phone_number: "584265510749",
         nick_name: "Pri",
-        dependents: []
+        dependents: [],
+        outside_country: false
     },
+    {
+        full_name: "Valentina Scope",
+        phone_number: "584148708583",
+        nick_name: "Pri",
+        dependents: [
+            {name: "Charles Scope"},
+            {name: "Valeria Scope"},
+            {name: "Christofer Scope"}
+        ],
+        outside_country: false
+    },
+    {
+        full_name: "Valentina Scope",
+        phone_number: "584148708583",
+        nick_name: "Pri",
+        dependents: [
+            {name: "Charles Scope"},
+            {name: "Valeria Scope"},
+            {name: "Christofer Scope"}
+        ],
+        outside_country: false
+    },
+    {
+        full_name: "Gabriel Ruiz",
+        phone_number: "58x0x0x0x0x0x0x",
+        nick_name: "Gabriel",
+        dependents: [
+            {name: "Roxana Sanchez"},
+            {name: "Abraham Ruiz"}
+        ],
+        outside_country: false
+    },
+    {
+        full_name: "Liliana Lucchesse",
+        phone_number: "58x0x0x0x0x0x0x",
+        nick_name: "Tia Liliana",
+        dependents: [
+            {name: "Gabriel Ruiz"}
+        ],
+        outside_country: false
+    },
+    {
+        full_name: "Jesús Mictil",
+        phone_number: "584248646412",
+        nick_name: "Pachus",
+        dependents: [],
+        outside_country: false
+    },
+    {
+        full_name: "Amanda Sanchez",
+        phone_number: "584148201549",
+        nick_name: "Mandy",
+        dependents: [],
+        outside_country: false
+    },
+    {
+        full_name: "Arlette Gomez",
+        phone_number: "584148078175",
+        nick_name: "Arlette",
+        dependents: [
+            {name: "Jesús Perez"},
+            {name: "Georgette Perez"}
+        ],
+        outside_country: false
+    },
+    {
+        full_name: "Luis Felipe Malaver",
+        phone_number: "584148495504",
+        nick_name: "Lucho",
+        dependents: [],
+        outside_country: false
+    },
+    {
+        full_name: "Maria Velasquez",
+        phone_number: "584120831055",
+        nick_name: "Marivi",
+        dependents: [
+            {name: "Camila Hernandez"},
+            {name: "Leomary Mesa"},
+            {name: "Angel Indaburo"},
+            {name: "Leonor Indaburo"},
+            {name: "Berta Gonzalez"},
+            {name: "Ibelice Mesa"},
+            {name: "Chiquinquira Gonzalez"},
+            {name: "Yvett Mesa"},
+        ],
+        outside_country: false
+    },
+    {
+        full_name: "Camila Hernandez",
+        phone_number: "58x0x0x0x0x0x0x",
+        nick_name: "Cami",
+        dependents: [],
+        outside_country: false
+    },
+    {
+        full_name: "Leomary Mesa",
+        phone_number: "58x0x0x0x0x0x0x",
+        nick_name: "Leo",
+        dependents: [
+            {name: "Ibelice Mesa"},
+            {name: "Yvett Mesa"},
+        ],
+        outside_country: false
+    },
+    {
+        full_name: "Angel Indaburo",
+        phone_number: "58x0x0x0x0x0x0x",
+        nick_name: "Angel",
+        dependents: [],
+        outside_country: false
+    },
+    {
+        full_name: "Leonor Indaburo",
+        phone_number: "58x0x0x0x0x0x0x",
+        nick_name: "Leo",
+        dependents: [
+            {name: "Angel Indaburo"},
+            {name: "Leonor Indaburo"},
+        ],
+        outside_country: false
+    },
+    {
+        full_name: "Berta Gonzalez",
+        phone_number: "58x0x0x0x0x0x0x",
+        nick_name: "Marivi",
+        dependents: [
+            {name: "Berta Gonzalez"},
+            {name: "Chiquinquira Gonzalez"},
+        ],
+        outside_country: false
+    },
+    {
+        full_name: "Ibelice Mesa",
+        phone_number: "58x0x0x0x0x0x0x",
+        nick_name: "Ibe",
+        dependents: [
+            {name: "Leomary Mesa"},
+            {name: "Ibelice Mesa"},
+            {name: "Yvett Mesa"},
+        ],
+        outside_country: false
+    },
+    {
+        full_name: "Chiquinquira Gonzalez",
+        phone_number: "58x0x0x0x0x0x0x",
+        nick_name: "Chiqui",
+        dependents: [
+            {name: "Berta Gonzalez"},
+            {name: "Chiquinquira Gonzalez"},
+            {name: "Airan Gonzalez"},
+        ],
+        outside_country: false
+    },
+    {
+        full_name: "Yvett Mesa",
+        phone_number: "584120831055",
+        nick_name: "Yvett",
+        dependents: [
+            {name: "Leomary Mesa"},
+            {name: "Ibelice Mesa"},
+        ],
+        outside_country: false
+    },
+    {
+        full_name: "Mariela Rojas",
+        phone_number: "584144555606",
+        nick_name: "Mariela",
+        dependents: [],
+        outside_country: false
+    },
+    {
+        full_name: "Mairelys Vera",
+        phone_number: "584143168527",
+        nick_name: "Mai",
+        dependents: [
+            {name: "Arturo Pinto"}
+        ],
+        outside_country: false
+    },
+    {
+        full_name: "Arturo Pinto",
+        phone_number: "584243503366",
+        nick_name: "Mai",
+        dependents: [
+            {name: "Mairelys Vera"}
+        ],
+        outside_country: false
+    },
+    {
+        full_name: "Vanesa Gonzalez",
+        phone_number: "58x0x0x0x0x0x0x",
+        nick_name: "Vane",
+        dependents: [
+            {name: "Viviana Makarem"}
+        ],
+        outside_country: false
+    },
+    {
+        full_name: "Maria Koury",
+        phone_number: "584148148118",
+        nick_name: "Friend",
+        dependents: [
+            {name: "Manuel Curi"}
+        ],
+        outside_country: false
+    },
+    {
+        full_name: "Marilena Rodriguez",
+        phone_number: "584166806368",
+        nick_name: "Marilena",
+        dependents: [
+            {name: "Luis Indriago"},
+            {name: "Luis Indriago Jr"}
+        ],
+        outside_country: false
+    },
+    {
+        full_name: "Luis Indriago",
+        phone_number: "58x0x0x0x0x0x0x",
+        nick_name: "Luis",
+        dependents: [
+            {name: "Marilena Rodriguez"},
+            {name: "Luis Indriago Jr"}
+        ],
+        outside_country: false
+    },
+    {
+        full_name: "Ofelia Ruiz",
+        phone_number: "584141993535",
+        nick_name: "Ofelia",
+        dependents: [
+            {name: "Victor Ruiz"}
+        ],
+        outside_country: false
+    },
+    {
+        full_name: "Fernando Romero",
+        phone_number: "58x0x0x0x0x0x0x",
+        nick_name: "Fer",
+        dependents: [
+            {name: "Gabriela Bravo"},
+            {name: "Christopher Romero"}
+        ],
+        outside_country: false
+    },
+    {
+        full_name: "Gabriela Bravo",
+        phone_number: "58x0x0x0x0x0x0x",
+        nick_name: "Gaby",
+        dependents: [
+            {name: "Fernando Romero"},
+            {name: "Christopher Romero"}
+        ],
+        outside_country: false
+    },
+    {
+        full_name: "Anny Rodriguez",
+        phone_number: "58x0x0x0x0x0x0x",
+        nick_name: "Anny",
+        dependents: [
+            {name: "Carli Lezama"}
+        ],
+        outside_country: false
+    },
+    {
+        full_name: "Dalila Perez",
+        phone_number: "58x0x0x0x0x0x0x",
+        nick_name: "Dalila",
+        dependents: [
+            {name: "River Fajardo"}
+        ],
+        outside_country: false
+    },
+    {
+        full_name: "Dinora Rodriguez",
+        phone_number: "58x0x0x0x0x0x0x",
+        nick_name: "Dino",
+        dependents: [
+            {name: "Miguel Hermoso"}
+        ],
+        outside_country: false
+    },
+    {
+        full_name: "Miguel Hermoso",
+        phone_number: "58x0x0x0x0x0x0x",
+        nick_name: "Miguel",
+        dependents: [
+            {name: "Dinora Rodriguez"}
+        ],
+        outside_country: false
+    },
+    {
+        full_name: "Jesus Portillo",
+        phone_number: "58x0x0x0x0x0x0x",
+        nick_name: "Porti",
+        dependents: [
+            {name: "Victor"} // ToDo: Fill
+        ],
+        outside_country: false
+    },
+    {
+        full_name: "Jaseri Matute",
+        phone_number: "58x0x0x0x0x0x0x",
+        nick_name: "Jase",
+        dependents: [],
+        outside_country: false
+    },
+    {
+        full_name: "Eliezer Caro",
+        phone_number: "58x0x0x0x0x0x0x",
+        nick_name: "Eliezer",
+        dependents: [],
+        outside_country: false
+    },
+    {
+        full_name: "Eduardo Gonzalez",
+        phone_number: "58x0x0x0x0x0x0x",
+        nick_name: "Eduardo",
+        dependents: [],
+        outside_country: false
+    },
+    {
+        full_name: "Luis Sanchez",
+        phone_number: "584127679315",
+        nick_name: "Luis",
+        dependents: [
+            {name: "Mariagel Borges"}
+        ],
+        outside_country: false
+    },
+    {
+        full_name: "Oriana Spangenberg",
+        phone_number: "34693292634",
+        nick_name: "Luis",
+        dependents: [
+            {name: "Roberto Sano"},
+            {name: "Flavia Sano"}
+        ],
+        outside_country: false
+    },
+    {
+        full_name: "Roberto Sano",
+        phone_number: "584243208271",
+        nick_name: "Luis",
+        dependents: [
+            {name: "Oriana Spangenberg"},
+            {name: "Flavia Sano"}
+        ],
+        outside_country: false
+    }
 ]
